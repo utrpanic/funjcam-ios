ad49b8637b5f946d732b58f65dabfd6a94689b0b
BoxJeon <utrpanic@gmail.com>
2016-07-19 14:59:19 +0900

    Create project.

---
+++ funjcam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  funjcam
+//
+//  Created by gurren-l on 2016. 7. 19..
+//  Copyright © 2016년 boxjeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
