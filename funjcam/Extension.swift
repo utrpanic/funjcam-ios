a7a179b4144732f864773ff4177844ff5238235a
BoxJeon <utrpanic@gmail.com>
2016-09-06 18:31:47 +0900

    extension 추가.

--- funjcam/Extension.swift
+++ funjcam/Extension.swift
@@ -9,22 +9,43 @@
 import SDWebImage
 
 extension String {
+    
     var urlEncoded: String? {
         return self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
     }
+    
+    var trim: String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    }
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+    func substring(from from: Int) -> String {
+        return self.substringWithRange(self.startIndex.advancedBy(from) ..< self.endIndex)
+    }
+    
+    func substring(to to: Int) -> String {
+        return self.substringWithRange(self.startIndex ..< self.startIndex.advancedBy(to))
+    }
+    
+    func substring(from from: Int, to: Int) -> String {
+        return self.substring(to: to).substring(from: from)
+    }
+    
 }
 
 extension UICollectionView {
+    
     func registerClassById(id: String) {
         self.registerNib(UINib(nibName: id, bundle: nil), forCellWithReuseIdentifier: id)
+    
     }
 }
 
-extension UICollectionViewCell {
-    class var id: String { return NSStringFromClass(self).componentsSeparatedByString(".").last! }
-}
-
 extension UIImageView {
+    
     func setImage(url url: String?, placeholder: UIImage?) {
         if let url = url?.urlEncoded {
             self.sd_setImageWithURL(NSURL(string: url)!)
@@ -32,16 +53,29 @@ extension UIImageView {
             self.image = placeholder
         }
     }
-}
-
-extension UITableViewCell {
-    class var id: String { return NSStringFromClass(self).componentsSeparatedByString(".").last! }
+    
 }
 
 extension UIView {
+    
+    class var id: String { return NSStringFromClass(self).componentsSeparatedByString(".").last! }
+    
     func drawBorder(color color: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1), width: CGFloat = 0.5) {
         self.layer.borderColor = color.CGColor
         self.layer.borderWidth = width
         self.clipsToBounds = true
     }
+    
+    var widthConstraint: NSLayoutConstraint? {
+        return self.constraints.filter({ (constraint) -> Bool in
+            return constraint.firstItem === self && constraint.firstAttribute == .Width && constraint.secondItem == nil
+        }).last
+    }
+    
+    var heightConstraint: NSLayoutConstraint? {
+        return self.constraints.filter({ (constraint) -> Bool in
+            return constraint.firstItem === self && constraint.firstAttribute == .Height && constraint.secondItem == nil
+        }).last
+    }
+    
 }
