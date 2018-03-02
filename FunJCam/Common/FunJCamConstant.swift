dfd411f2fab3d3f7fae614b68421754294408418
Box Jeon <utrpanic@gmail.com>
2018-03-03 03:33:26 +0900

    SwiftLint 도입. Rule black list 방식.

--- FunJCam/Common/FunJCamConstant.swift
+++ FunJCam/Common/FunJCamConstant.swift
@@ -5,22 +5,18 @@
 //  Created by gurren-l on 2017. 11. 14..
 //  Copyright © 2017년 the42apps. All rights reserved.
 //
-
-struct FJConstant {
     
-    struct device {
-        static var isPhone: Bool { return UIDevice.current.userInterfaceIdiom == .phone }
-        static var isPad: Bool { return UIDevice.current.userInterfaceIdiom == .pad }
-        static var hasNotch: Bool {
-            return self.notchHeight > 0
-        }
-        static var notchHeight: CGFloat {
-            if #available(iOS 11.0, *) {
-                return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0
-            } else {
-                return 0
-            }
+struct FJDevice {
+    static var isPhone: Bool { return UIDevice.current.userInterfaceIdiom == .phone }
+    static var isPad: Bool { return UIDevice.current.userInterfaceIdiom == .pad }
+    static var hasNotch: Bool {
+        return self.notchHeight > 0
+    }
+    static var notchHeight: CGFloat {
+        if #available(iOS 11.0, *) {
+            return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0
+        } else {
+            return 0
         }
     }
-    
 }
--- FunJCam/Networking/Code.swift
+++ FunJCam/Networking/Code.swift
@@ -30,5 +30,4 @@ enum Code {
             self = .none
         }
     }
-    
 }
