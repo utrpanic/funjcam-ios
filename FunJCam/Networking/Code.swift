270ba8bfef7ed214b064d08d3447d91e7d4461e4
Box Jeon <utrpanic@gmail.com>
2018-05-04 00:04:08 +0900

    Pod update.

--- FunJCam/Networking/Code.swift
+++ FunJCam/Networking/Code.swift
@@ -1,39 +1,11 @@
+//
+//  Code.swift
+//  FunJCam
+//
+//  Created by boxjeon on 2018. 2. 10..
+//  Copyright © 2018년 the42apps. All rights reserved.
+//
 
-enum Code {
-    
-    case none
-    case undefined(code: Int)
-    case jsonError(message: String)
-    
-    case ok
-    case created
+class Code: HttpStatusCode {
 
-    case unauthorized
-    case badRequest
-    case forbidden
-    case notFound
-    
-    private static let map: [Int: Code] = [
-        200: .ok,
-        201: .created,
-        400: .badRequest,
-        401: .unauthorized,
-        403: .forbidden,
-        404: .notFound,
-    ]
-    
-    var isSucceed: Bool {
-        switch self {
-        case .ok, .created: return true
-        default: return false
-        }
-    }
-    
-    init(value: Int?) {
-        if let value = value {
-            self = Code.map[value] ?? .undefined(code: value)
-        } else {
-            self = .none
-        }
-    }
 }
