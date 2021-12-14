a65202f1e3ce0c8e21c31cdd81b7e2fc2d13c020
box-jeon <utrpanic@gmail.com>
2021-12-14 09:20:55 +0900

    Usecase. Add Bookmark usecase.

--- Domain/Entity/BookmarkImage.swift
+++ Domain/Entity/BookmarkImage.swift
@@ -1,16 +1,18 @@
-import Foundation
+import UIKit
 
-public struct RecentImage {
+public struct BookmarkImage {
   
   public var id: Int
   public var name: String
   public var url: URL?
+  public var image: UIImage
   public var createdAt: Date
   
-  public init(id: Int, name: String, urlString: String, createdAt: Date) {
+  public init(id: Int, name: String, urlString: String, image: UIImage, createdAt: Date) {
     self.id = id
     self.name = name
     self.url = URL(string: urlString)
+    self.image = image
     self.createdAt = createdAt
   }
 }
